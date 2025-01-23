df6d4c2569f2143efedc32b4459e187663c167aa
Alev <alexandergallego1406mail.com>
2025-01-22 20:32:33 -0500

    Actu

--- src/app/presentacion/presentacion.page.ts
+++ src/app/presentacion/presentacion.page.ts
@@ -14,4 +14,7 @@ export class PresentacionPage implements OnInit {
   ngOnInit() {
   }
 
+  finish() {
+    console.log('Finish');
+  }
 }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -4,6 +4,7 @@ import{
   FormControl,
   Validators,
   FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -16,21 +17,53 @@ export class RegistroPage implements OnInit {
 
   formularioRegistro: FormGroup;
 
-  constructor(public fb: FormBuilder) {
-    this.formularioRegistro = this.fb.group({
-      'nombre': new FormControl("", Validators.required),
-      'apellido': new FormControl("", Validators.required),
-      'email': new FormControl("", Validators.required),
-      'pws': new FormControl("", Validators.required),
-      'pws2': new FormControl("", Validators.required),
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController){
+      this.formularioRegistro = this.fb.group({
+        'nombre': new FormControl("", Validators.required),
+        'apellido': new FormControl("", Validators.required),
+        'email': new FormControl("", Validators.required),
+        'pass': new FormControl("", Validators.required),
+        'pass2': new FormControl("", Validators.required),
+      
     });
   }
   
   ngOnInit() {
   }
 
-  registrarse(){
+  async registrarse(){
     console.log("registro");
-  }
+    var f = this.formularioRegistro.value;
 
+    if(this.formularioRegistro.invalid){
+      console.log("invalido");
+      const alert = await this.alertController.create({
+        header: 'Erro de registro',
+        subHeader: 'Error 1',
+        message: 'Tienes q llenar todos los campos',
+        buttons: ['Aceptar'],
+      });
+  
+      await alert.present();
+      return;
+
+    }else{
+      //Validaciones del formulario
+      console.log("valido");
+      //si las contraseñas no son iguales
+      if(f.pws != f.pws2){  
+      }
+
+    }
+    
+    var user = {
+      nombre: f.nombre,
+      psw: f.pws
+    }
+
+    localStorage.setItem('usuario', JSON.stringify(user));
+
+    
+  }
 }
